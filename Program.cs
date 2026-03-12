986d2ba4e4ac021526e12b9488da4167d572ac73
Bryson <bryson.leon-menjivar@students.snow.edu>
2026-03-11 23:10:34 -0600

    load the map text file

--- Program.cs
+++ Program.cs
@@ -5,3 +5,13 @@ Console.WriteLine("Welcome to the Maze Game!");
 Console.WriteLine("Reach the * to win. Press ESC to quit.");
 Console.WriteLine("Press any key to start...");
 Console.ReadKey(true);
+
+// load the map file and give error if can't find
+string fileName = "map.txt";
+if (!File.Exists(fileName))
+{
+    Console.WriteLine("Error: map.txt not found!");
+    return;
+}
+
+string[] mazeData = File.ReadAllLines(fileName);
