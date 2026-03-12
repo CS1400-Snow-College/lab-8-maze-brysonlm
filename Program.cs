de863fb128b7caa00d6723651170f808d7f367c0
Bryson <bryson.leon-menjivar@students.snow.edu>
2026-03-11 22:59:40 -0600

    Added a greeting

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// Bryson, 03/11/2026, Lab 8: Maze
+
+// nice greeting
+Console.WriteLine("Welcome to the Maze Game!");
+Console.WriteLine("Reach the * to win. Press ESC to quit.");
+Console.WriteLine("Press any key to start...");
+Console.ReadKey(true);
